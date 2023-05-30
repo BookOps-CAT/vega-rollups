ded0a41bf92d7b3fecf9b19c53e154772980036f
Tomek <klingaroo@gmail.com>
2023-05-30 09:57:45 -0400

    blackened

--- copy_number.py
+++ copy_number.py
@@ -6,11 +6,13 @@ from pymarc import Field
 def normalize_value(value: str) -> str:
     return "".join(x for x in value if x.isalnum())
 
+
 def find_digits(value: str) -> str:
     for c in value.split(" "):
         norm_c = normalize_value(c)
         if norm_c.isdigit():
             return norm_c
+
 
 def get_number(field: Field) -> Optional[str]:
     """
