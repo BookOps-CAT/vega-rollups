813c42786f8513af0eec69313599625520123643
Tomek <klingaroo@gmail.com>
2023-05-30 15:13:48 -0400

    init modification of 240

--- copy_number.py
+++ copy_number.py
@@ -3,6 +3,10 @@ from typing import Optional
 from pymarc import Field
 
 
+def determine_subfield_n_position(field: Field) -> int:
+    pass
+
+
 def normalize_value(value: str) -> str:
     return "".join(x for x in value if x.isalnum())
 
@@ -27,3 +31,16 @@ def get_number(field: Field) -> Optional[str]:
         return number
     except KeyError:
         return None
+
+
+def modify_uniform_title(number: str, field: Field) -> Field:
+    """
+    Inserts number into the subfield $n of 240 field
+    """
+    if field.tag != "240":
+        raise ValueError("Invalid MARC tag passed. Only 240 is accepted.")
+
+    pos = determine_subfield_n_position(field)
+    field.add_subfield("n", number, pos)
+
+    return field
