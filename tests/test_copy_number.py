950217aaa0b78b0e121be91d9d5a0a4a9e104d06
klinga <klingaroo@gmail.com>
2023-06-01 09:20:12 -0400

    add tests for modify_uniform_title

--- tests/test_copy_number.py
+++ tests/test_copy_number.py
@@ -50,15 +50,15 @@ def test_normalize_value(arg, expectation):
     assert normalize_value(arg) == expectation
 
 
-def test_get_number_invalid_tag(stub_title_no_number):
-    stub_title_no_number.tag = "246"
+def test_get_number_invalid_tag(stub_245_no_number):
+    stub_245_no_number.tag = "246"
 
     with pytest.raises(ValueError):
-        get_number(stub_title_no_number)
+        get_number(stub_245_no_number)
 
 
-def test_get_number_no_subfield(stub_title_no_number):
-    assert (get_number(stub_title_no_number)) is None
+def test_get_number_no_subfield(stub_245_no_number):
+    assert get_number(stub_245_no_number) is None
 
 
 @pytest.mark.parametrize(
@@ -73,14 +73,24 @@ def test_get_number_no_subfield(stub_title_no_number):
     ],
 )
 def test_find_digits(arg, expectation):
-    assert (find_digits(arg)) == expectation
+    assert find_digits(arg) == expectation
 
 
-def test_modify_uniform_title_missing_240(stub_title_with_number):
+def test_modify_uniform_title_missing_240(stub_245_with_number):
     with pytest.raises(ValueError):
-        modify_uniform_title("5", stub_title_with_number)
+        modify_uniform_title("5", stub_245_with_number)
 
 
 def test_modify_uniform_title(stub_240):
-    field = modify_uniform_title("5", stub_240, 1)
-    print(field)
+    field = modify_uniform_title("5", stub_240)
+    assert isinstance(field, Field) is True
+    assert str(field) == "=240  10$aFoo.$n5.$lEnglish"
+
+
+def test_modify_uniform_title_when_field_is_None():
+    with pytest.raises(AttributeError):
+        assert modify_uniform_title(5, None)
+
+
+def test_modify_uniform_title_when_number_is_None(stub_240):
+    assert modify_uniform_title(None, stub_240) is None
