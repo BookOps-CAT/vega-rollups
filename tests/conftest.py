f28a0df334c5ae16028885d104d993c91dd59ab3
Tomek <klingaroo@gmail.com>
2023-06-20 11:11:23 -0400

    add stub_bib

--- tests/conftest.py
+++ tests/conftest.py
@@ -39,3 +39,39 @@ def stub_240():
             Subfield(code="l", value="English"),
         ],
     )
+
+
+@pytest.fixture
+def stub_bib(stub_240, stub_245_with_number):
+    bib = Record()
+    bib.leader = "00000nam a2200000u  4500"
+    bib.add_field(Field(tag="008", data="20230616s        xx            000 u eng d"))
+    bib.add_field(
+        Field(tag="020", indicators=[" ", " "], subfields=[Subfield("a", "1234")])
+    )
+    bib.add_field(
+        Field(
+            tag="100", indicators=["1", "0"], subfields=[Subfield("a", "Smith, John.")]
+        )
+    )
+    bib.add_field(stub_240)
+    bib.add_field(stub_245_with_number)
+    bib.add_field(
+        Field(
+            tag="264",
+            indicators=[" ", "1"],
+            subfields=[
+                Subfield("a", "New York:"),
+                Subfield("b", "Orion,"),
+                Subfield("c", "2023."),
+            ],
+        )
+    )
+    bib.add_field(
+        Field(
+            tag="300",
+            indicators=[" ", " "],
+            subfields=[Subfield("a", "250 pages;"), Subfield("c", "23 cm")],
+        )
+    )
+    return bib
